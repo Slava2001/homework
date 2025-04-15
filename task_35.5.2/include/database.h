b352103982dd18f715d6b882115897f7fcc31897
Slava2001 <45189467+Slava2001@users.noreply.github.com>
2025-04-15 14:15:23 +0000

    task_35.5.2: fix doxy

--- task_35.5.2/include/database.h
+++ task_35.5.2/include/database.h
@@ -17,19 +17,19 @@ public:
     /// @throw nlohmann::json::parse_error if database file is not valid json
     Database(const char *path_to_database);
 
-    /// @brief Find film by first name
+    /// @brief Find actors by first name
     /// @param name actor first name
     /// @return map where key is actor and value is info about his roles
     std::map<Actor, ActorInfo> find_actor_by_first_name(const char *first_name) const;
 
-    /// @brief Find film by last name
+    /// @brief Find actors by last name
     /// @param name actor last name
     /// @return map where key is actor and value is info about his roles
     std::map<Actor, ActorInfo> find_actor_by_last_name(const char *last_name) const;
 private:
     std::map<std::string, Film>_films;
 
-    /// @brief Find film by predicate
+    /// @brief Find actors by predicate
     /// @param predicate predicate to find film
     /// @return map where key is actor and value is info about his roles
     std::map<Actor, ActorInfo> find_actor_by_predicate(

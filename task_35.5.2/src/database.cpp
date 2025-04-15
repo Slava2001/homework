ebadeca90852a7bb894b5572be13550e7ace72a9
Slava2001 <45189467+Slava2001@users.noreply.github.com>
2025-04-15 09:45:53 +0000

    task-35.5.2-3: fix fmt

--- task_35.5.2/src/database.cpp
+++ task_35.5.2/src/database.cpp
@@ -4,46 +4,46 @@
 #include "database.h"
 
 Database::Database(const char *path_to_database) {
-  std::ifstream cfg_file(path_to_database);
-  if (!cfg_file.is_open()) {
-    throw std::runtime_error("Failed to open file");
-  }
-  _films = nlohmann::json::parse(cfg_file);
-  cfg_file.close();
+    std::ifstream cfg_file(path_to_database);
+    if (!cfg_file.is_open()) {
+        throw std::runtime_error("Failed to open file");
+    }
+    _films = nlohmann::json::parse(cfg_file);
+    cfg_file.close();
 }
 
 std::map<Actor, ActorInfo> Database::find_actor_by_predicate(
-    std::function<bool(const Actor *)> predicate) const {
-  std::map<Actor, ActorInfo> result;
-  for (auto &film : _films) {
-    for (auto &character : film.second.main_characters) {
-      if (predicate(&character.actor)) {
-        auto actor_info = result.find(character.actor);
-        if (actor_info != result.end()) {
-          auto film_info = actor_info->second.films_map.find(film.first);
-          if (film_info != actor_info->second.films_map.end()) {
-            film_info->second.push_back(character);
-          } else {
-            actor_info->second.films_map[film.first] = {character};
-          }
-        } else {
-          result[character.actor] =
-              ActorInfo{.films_map = {{film.first, {character}}}};
+        std::function<bool(const Actor *)> predicate) const {
+    std::map<Actor, ActorInfo> result;
+    for (auto &film : _films) {
+        for (auto &character : film.second.main_characters) {
+            if (predicate(&character.actor)) {
+                auto actor_info = result.find(character.actor);
+                if (actor_info != result.end()) {
+                    auto film_info = actor_info->second.films_map.find(film.first);
+                    if (film_info != actor_info->second.films_map.end()) {
+                        film_info->second.push_back(character);
+                    } else {
+                        actor_info->second.films_map[film.first] = {character};
+                    }
+                } else {
+                    result[character.actor] =
+                            ActorInfo{.films_map = {{film.first, {character}}}};
+                }
+            }
         }
-      }
     }
-  }
-  return result;
+    return result;
 }
 
 std::map<Actor, ActorInfo>
 Database::find_actor_by_first_name(const char *first_name) const {
-  return this->find_actor_by_predicate(
-      [&](const Actor *a) -> bool { return a->first_name == first_name; });
+    return this->find_actor_by_predicate(
+            [&](const Actor *a) -> bool { return a->first_name == first_name; });
 }
 
 std::map<Actor, ActorInfo>
 Database::find_actor_by_last_name(const char *last_name) const {
-  return this->find_actor_by_predicate(
-      [&](const Actor *a) -> bool { return a->last_name == last_name; });
+    return this->find_actor_by_predicate(
+            [&](const Actor *a) -> bool { return a->last_name == last_name; });
 }
--- task_35.5.2/src/main.cpp
+++ task_35.5.2/src/main.cpp
@@ -4,66 +4,66 @@
 void show_found_actors(std::map<Actor, ActorInfo> actors);
 
 int main() {
-  std::cout << "Loading database..." << std::endl;
-  Database base("../database.json");
-  std::cout << "Loaded!" << std::endl;
+    std::cout << "Loading database..." << std::endl;
+    Database base("../database.json");
+    std::cout << "Loaded!" << std::endl;
 
-  while (true) {
-    std::cout << "Select option:" << std::endl
-              << "1. Find actor by first name" << std::endl
-              << "2. Find actor by last name" << std::endl
-              << "3. Exit" << std::endl;
-    int option;
-    std::cin >> option;
-    if (std::cin.fail()) {
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      std::cout << "Invalid input. Please enter a number." << std::endl;
-      continue;
-    }
-    if (option < 1 || option > 3) {
-      std::cout << "Invalid option. Please enter a number between 1 and 3."
-                << std::endl;
-      continue;
-    }
+    while (true) {
+        std::cout << "Select option:" << std::endl
+                  << "1. Find actor by first name" << std::endl
+                  << "2. Find actor by last name" << std::endl
+                  << "3. Exit" << std::endl;
+        int option;
+        std::cin >> option;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input. Please enter a number." << std::endl;
+            continue;
+        }
+        if (option < 1 || option > 3) {
+            std::cout << "Invalid option. Please enter a number between 1 and 3."
+                      << std::endl;
+            continue;
+        }
 
-    switch (option) {
-    case 1: {
-      std::cout << "Enter first name: ";
-      std::string first_name;
-      std::cin >> first_name;
-      show_found_actors(base.find_actor_by_first_name(first_name.c_str()));
-      break;
+        switch (option) {
+        case 1: {
+            std::cout << "Enter first name: ";
+            std::string first_name;
+            std::cin >> first_name;
+            show_found_actors(base.find_actor_by_first_name(first_name.c_str()));
+            break;
+        }
+        case 2: {
+            std::cout << "Enter last name: ";
+            std::string last_name;
+            std::cin >> last_name;
+            show_found_actors(base.find_actor_by_last_name(last_name.c_str()));
+            break;
+        }
+        case 3: {
+            return EXIT_SUCCESS;
+        }
+        }
     }
-    case 2: {
-      std::cout << "Enter last name: ";
-      std::string last_name;
-      std::cin >> last_name;
-      show_found_actors(base.find_actor_by_last_name(last_name.c_str()));
-      break;
-    }
-    case 3: {
-      return EXIT_SUCCESS;
-    }
-    }
-  }
 }
 
 void show_found_actors(std::map<Actor, ActorInfo> actors) {
-  if (actors.empty()) {
-    std::cout << "No actors found." << std::endl;
-    return;
-  }
-  for (auto &actor_info : actors) {
-    std::cout << "Actor: " << actor_info.first.first_name << " "
-              << actor_info.first.last_name << std::endl;
-    for (auto &films : actor_info.second.films_map) {
-      std::cout << "Film: " << films.first << std::endl;
-      for (auto &character : films.second) {
-        std::cout << "    Role: " << character.name << std::endl
-                  << "    Description: " << character.description << std::endl
-                  << std::endl;
-      }
+    if (actors.empty()) {
+        std::cout << "No actors found." << std::endl;
+        return;
     }
-  }
+    for (auto &actor_info : actors) {
+        std::cout << "Actor: " << actor_info.first.first_name << " "
+                               << actor_info.first.last_name << std::endl;
+        for (auto &films : actor_info.second.films_map) {
+            std::cout << "Film: " << films.first << std::endl;
+            for (auto &character : films.second) {
+                std::cout << "    Role: " << character.name << std::endl
+                          << "    Description: " << character.description << std::endl
+                          << std::endl;
+            }
+        }
+    }
 }

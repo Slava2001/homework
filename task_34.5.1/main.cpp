6c6b53761aa64122b22e58aa05367659a61bf1c7
Slava2001 <slavakaplya20011501@gmail.com>
2025-04-23 21:50:31 +0300

    task_34.5.1: fix fmt

--- task_34.5.1/main.cpp
+++ task_34.5.1/main.cpp
@@ -1,12 +1,12 @@
 #include <QApplication>
 #include <QPushButton>
 
-int main(int args, char** argv){
+int main(int args, char **argv)
+{
     QApplication a(args, argv);
     QPushButton button(
-        "Hello, world!\nThis is my first button in QT", 
-        nullptr
-    );
+        "Hello, world!\nThis is my first button in QT",
+        nullptr);
     button.resize(200, 100);
     button.show();
     return QApplication::exec();

95d63725a3de40e0836aa5870e9805806fc80868
Slava2001 <slavakaplya20011501@gmail.com>
2025-04-23 21:49:42 +0300

    task_34.5.1

---
+++ task_34.5.1/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int args, char** argv){
+    QApplication a(args, argv);
+    QPushButton button(
+        "Hello, world!\nThis is my first button in QT", 
+        nullptr
+    );
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
+}
